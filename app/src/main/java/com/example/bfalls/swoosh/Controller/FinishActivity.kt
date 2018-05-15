2dbc23169efaec30472726568072acba34eb6967
Bryan Falls <bfalls1587@gmail.com>
2018-05-15 13:38:42 -0400

    all layouts added

---
+++ app/src/main/java/com/example/bfalls/swoosh/Controller/FinishActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bfalls.swoosh.Controller
+
+import android.os.Bundle
+import com.example.bfalls.swoosh.R
+import com.example.bfalls.swoosh.Utilities.EXTRA_LEAGUE
+import com.example.bfalls.swoosh.Utilities.EXTRA_SKILL
+import kotlinx.android.synthetic.main.activity_finish.*
+
+class FinishActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish)
+
+        val league = intent.getStringExtra(EXTRA_LEAGUE)
+        val skill = intent.getStringExtra(EXTRA_SKILL)
+
+        searchLeaguesText.text = "Looking for $league $skill league near you..."
+    }
+}
---
+++ app/src/main/java/com/example/bfalls/swoosh/Utilities/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.bfalls.swoosh.Utilities
+
+const val EXTRA_LEAGUE = "league"
+const val EXTRA_SKILL = "skill"
